12362b57fcbe79f5312bc4d88a96248ba8c6b206
Ravi Pingat <pingatravi@gmail.com>
2020-08-31 16:59:48 +0530

    Let users push build status to their own channels/groups
    *let it be optional

--- script_build.sh
+++ script_build.sh
@@ -9,6 +9,7 @@ OUT_PATH="out/target/product/$device_codename"
 tg_username=@
 ROM_ZIP=Rom*.zip
 folderid=gdrive_folder_id
+tgsend_conf=example.conf
 START=$(date +%s)
 
 # Colors makes things beautiful
@@ -31,6 +32,7 @@ read -r -d '' msg <<EOT
 Check progress <a href="${BUILD_URL}console">HERE</a>
 EOT
 telegram-send --format html "$msg"
+#telegram-send --format html "$msg" --config ~/${tgsend_conf}
 # Ccache
 if [ "$use_ccache" = "yes" ];
 then
@@ -101,6 +103,7 @@ read -r -d '' suc <<EOT
 Check console output <a href="${BUILD_URL}console">HERE</a>
 EOT
 telegram-send --format html "$suc"
+#telegram-send --format html "$suc" --config ~/${tgsend_conf}
 else
 # Send message to TG
 read -r -d '' fail <<EOT
@@ -113,4 +116,5 @@ read -r -d '' fail <<EOT
 Check what caused build to fail <a href="${BUILD_URL}console">HERE</a>
 EOT
 telegram-send --format html "$fail"
+#telegram-send --format html "$fail" --config ~/${tgsend_conf}
 fi
