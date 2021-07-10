e8175e93c0f4ccb7181b2195f5a96c1969b922bf
Ravi Pingat <pingatravi@gmail.com>
2021-07-10 18:30:38 +0530

    Update cred.sh

--- cred.sh
+++ cred.sh
@@ -1,27 +1,14 @@
 GIT_USERNAME="$(git config --get user.name)"
-
 GIT_EMAIL="$(git config --get user.email)"
-
 echo "Configuring git"
-
 if [[ -z ${GIT_USERNAME} ]]; then
-
     echo -n "Enter your name: "
-
     read -r NAME
-
     git config --global user.name "${NAME}"
-
 fi
-
 if [[ -z ${GIT_EMAIL} ]]; then
-
     echo -n "Enter your email: "
-
     read -r EMAIL
-
     git config --global user.email "${EMAIL}"
-
 fi
-
 git config --global credential.helper "cache --timeout=7200"
